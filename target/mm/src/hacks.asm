caacc17686c7f136327af36a9f7b010bf6c702db
saneki <s@neki.me>
2019-10-27 21:40:50 -0500

    B-button fix for non-transformation masks on DPad

--- target/mm/src/hacks.asm
+++ target/mm/src/hacks.asm
@@ -1,6 +1,12 @@
 ;==================================================================================================
-; DPad Not-on-C-Buttons Fix
+; DPad Not-on-C-Buttons Fixes
 ;==================================================================================================
+
+; Remove C button check for equipped mask, fixes using B button item (usually sword).
+; Replaces:
+;   jal 0x8074D19C
+.orga 0xBF9E14 ; In memory: 0x8074D314
+    nop
 
 ; For some items, if UseItem is called without said item being on the C-buttons,
 ; it will be automatically called again to unequip. Prevents this.
