8bb225ccc58d854c672399ef92502b28eaef57fc
saneki <s@neki.me>
2019-10-28 05:50:59 -0500

    Cleaner implementation with DPAD_STATE

--- target/mm/c/dpad.h
+++ target/mm/c/dpad.h
@@ -1,6 +1,12 @@
 #ifndef DPAD_H
 #define DPAD_H
 
+typedef enum {
+    DPAD_STATE_TYPE_DISABLED, // 0
+    DPAD_STATE_TYPE_ENABLED,  // 1, enabled with DPAD_CONFIG values
+    DPAD_STATE_TYPE_DEFAULTS, // 2, enabled with DPAD_DEFAULT values
+} DPAD_STATE_TYPE;
+
 void dpad_init();
 void handle_dpad();
 void draw_dpad();
