8300f980e7c09ae4033e68542cb6808511a67a95
saneki <s@neki.me>
2019-11-03 14:11:08 -0600

    Fix restoring button states
    Properly backs up and restores state before and after call to
    z64_UpdateButtonUsability, including some other fields which may be
    modified. This seems to fix the button drawing in archery games.

--- assembly/target/mm/c/item.c
+++ assembly/target/mm/c/item.c
@@ -2,15 +2,24 @@
 #include "z64.h"
 
 void check_item_usability(bool *dest, uint8_t b, uint8_t c1, uint8_t c2, uint8_t c3) {
-    uint8_t previous[4];
+    uint8_t previous[4], prevstates[5];
 
-    // Backup button alpha transition
+    // Backup modified fields
+    uint8_t game_state = z64_file.game_state;
+    uint8_t pre_game_state = z64_file.pre_game_state;
     uint8_t alpha_transition = z64_file.alpha_transition;
+    uint8_t sub_game_state = z64_file.sub_game_state;
 
     // Backup button items
     previous[0] = z64_file.b_button;
     for (int i = 0; i < 3; i++)
         previous[i+1] = z64_file.c_buttons[i];
+
+    // Backup button states
+    prevstates[0] = z64_file.a_button_usable;
+    prevstates[1] = z64_file.b_button_usable;
+    for (int i = 0; i < 3; i++)
+        prevstates[i+2] = z64_file.c_buttons_usable[i];
 
     z64_file.b_button = b;
     z64_file.c_buttons[0] = c1;
@@ -29,11 +38,17 @@ void check_item_usability(bool *dest, uint8_t b, uint8_t c1, uint8_t c2, uint8_t
     for (int i = 0; i < 3; i++)
         z64_file.c_buttons[i] = previous[i+1];
 
-    // Restore states
-    z64_UpdateButtonUsability(&z64_ctxt);
+    // Restore button states
+    z64_file.a_button_usable = prevstates[0];
+    z64_file.b_button_usable = prevstates[1];
+    for (int i = 0; i < 3; i++)
+        z64_file.c_buttons_usable[i] = prevstates[i+2];
 
-    // Restore alpha transition
+    // Restore modified fields
+    z64_file.game_state = game_state;
+    z64_file.pre_game_state = pre_game_state;
     z64_file.alpha_transition = alpha_transition;
+    z64_file.sub_game_state = sub_game_state;
 }
 
 bool check_c_item_usable(uint8_t c) {
