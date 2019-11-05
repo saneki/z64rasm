a7fc982a2d29af48e10077b87e584cd895210e16
saneki <s@neki.me>
2019-11-05 07:18:33 -0600

    Comment out unused before/after frame functions
    These are unused and after_game_state_update in particular may be
    crashing when used on console, for some reason.

--- assembly/target/mm/src/every_frame.asm
+++ assembly/target/mm/src/every_frame.asm
@@ -1,19 +1,19 @@
-before_game_state_update_hook:
-    ; Displaced code
-    sb      r0, 0x00A3 (a1)
-    sw      a1, 0x001C (sp)
-
-    addiu   sp, sp, -0x18
-    sw      ra, 0x10 (sp)
-
-    jal     before_game_state_update
-    nop
-
-    lw      ra, 0x10 (sp)
-    addiu   sp, sp, 0x18
-
-    jr      ra
-    nop
+;before_game_state_update_hook:
+;    ; Displaced code
+;    sb      r0, 0x00A3 (a1)
+;    sw      a1, 0x001C (sp)
+;
+;    addiu   sp, sp, -0x18
+;    sw      ra, 0x10 (sp)
+;
+;    jal     before_game_state_update
+;    nop
+;
+;    lw      ra, 0x10 (sp)
+;    addiu   sp, sp, 0x18
+;
+;    jr      ra
+;    nop
 
 before_non_menu_update_hook:
     addiu   sp, sp, -0x10
--- assembly/target/mm/src/hooks.asm
+++ assembly/target/mm/src/hooks.asm
@@ -6,17 +6,17 @@
 ; Replaces:
 ;   sb      r0, 0x00A3 (a1)
 ;   sw      a1, 0x001C (sp)
-.orga 0xB5A8D4 ; In memory: 0x80174874
-    jal     before_game_state_update_hook
-    nop
+;.orga 0xB5A8D4 ; In memory: 0x80174874
+;    jal     before_game_state_update_hook
+;    nop
 
 ; Runs after the game state update function
 ; Replaces:
 ;   jr      ra
 ;   nop
-.orga 0xB5A8F8 ; In memory: 0x80174898
-    j       after_game_state_update
-    nop
+;.orga 0xB5A8F8 ; In memory: 0x80174898
+;    j       after_game_state_update
+;    nop
 
 ;==================================================================================================
 ; Non-menu processing hooks
