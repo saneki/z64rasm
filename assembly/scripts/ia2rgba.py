fafaf2bf41ee972936139f66183dde9c6a353ecb
saneki <s@neki.me>
2019-11-05 09:23:12 -0600

    Use 32-bit RGBA for D-Pad texture instead of 16-bit IA
    In scenes with hidden objects, when using the Lens of Truth the D-Pad
    texture does not display correctly as IA (changes to a white box until
    pause). Using RGBA instead seems to fix this.

---
+++ assembly/scripts/ia2rgba.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+import struct
+
+def unpack_ia(value):
+    """ Unpack 16-bit IA value to RGBA values. """
+    intensity = value >> 8
+    alpha = value & 0xFF
+    return (intensity, intensity, intensity, alpha)
+
+def pack32(values):
+    """ Pack RGBA values to a 32-bit 8/8/8/8 integer."""
+    r, g, b, a = values
+    return ((r & 0xFF) << 24) | ((g & 0xFF) << 16) | ((b & 0xFF) << 8) | (a & 0xFF)
+
+def get_parser():
+    """ Get the argument parser. """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='rgba5551 texture input file')
+    parser.add_argument('output', help='rgba8888 texture output file')
+    return parser
+
+def main():
+    """ Main function. """
+    args = get_parser().parse_args()
+
+    with open(args.input, 'rb') as infile:
+        with open(args.output, 'wb') as outfile:
+            while True:
+                indata = infile.read(2)
+                if len(indata) == 0:
+                    break
+                value = struct.unpack('>H', indata)[0]
+                result = pack32(unpack_ia(value))
+                outdata = struct.pack('>I', result)
+                outfile.write(outdata)
+
+if __name__ == '__main__':
+    main()
