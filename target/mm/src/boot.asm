ec231787badce2e63f80371d0019029926f74099
saneki <s@neki.me>
2019-11-10 07:53:12 -0600

    Move payload to 0x807A4000 for console fix
    I mean who the hell knows why but whatever.

--- target/mm/src/boot.asm
+++ target/mm/src/boot.asm
@@ -3,6 +3,10 @@
 ;  However, may be able to use the following region: [0x80780000, 0x807DA800).
 ;  Everything after 0x807DA800 seems to be used by something until the RAM end (0x80800000).
 ;
+; Console / EverDrive notes:
+;  Despite never being used on emulator, apparently [0x80780000, 0x807A3FCC) is written to,
+;  or unstable or something. [0x807A4000, 0x807DA800) does seem to be unused/stable.
+;
 ; Add dmatable entries for new code
 ; Remove the unused files at the bottom the DMA Table
 ;   - this isn't strictly necessary, but adds flexibility for the future
@@ -28,8 +32,8 @@
     sw      a0, 0x0340 (sp)
 
     ; Load first code file from ROM
-    lui     a0, 0x8078
-    ori     a0, a0, 0x4600
+    lui     a0, 0x807A
+    ori     a0, a0, 0x4000
     li      a2, PAYLOAD_END - PAYLOAD_START
     jal     0x80080C90
     lui     a1, 0x02F0
