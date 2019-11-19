147bdae8a2cb09098ade1b5e1513e6a696cf0865
saneki <s@neki.me>
2019-11-18 19:01:20 -0600

    Return from handle_dpad hooked function early if item used
    This fixes a bug when trying to use an item with no B button item set,
    and when equipped with a mask which uses the B button (Blast, Bremen,
    Kamaro). See comment for further details.

--- target/mm/c/dpad.h
+++ target/mm/c/dpad.h
@@ -1,6 +1,8 @@
 #ifndef DPAD_H
 #define DPAD_H
 
+#include <stdbool.h>
+
 // Most of these states get checked by the "UseItem" function itself.
 // We do need to handle some of them though.
 #define DPAD_ACTION_STATE1 (        \
@@ -16,7 +18,7 @@ typedef enum {
 
 void do_dpad_per_game_frame();
 void dpad_init();
-void handle_dpad();
+bool handle_dpad();
 void draw_dpad();
 
 #endif
--- target/mm/src/dpad.asm
+++ target/mm/src/dpad.asm
@@ -22,6 +22,9 @@ dpad_handle:
     jal     handle_dpad
     nop
 
+    bnez    v0, dpad_handle_caller_return
+    nop
+
     ; Displaced code (restore to s0)
     lw      s0, 4 (sp)
 
@@ -30,3 +33,25 @@ dpad_handle:
 
     jr ra
     nop
+
+; Used to return early from the hooked function 0x8074D29C if we called z64_UseItem.
+; This is because z64_UseItem sets a byte (0x803FFDB0 +0x14A) which is the item Link currently has out.
+; Because our hook is at the start of the function, this value may be set earlier than expected.
+;
+; Specifically, the branch at 0x8074D408 won't be taken, and can lead to a crash in specific conditions.
+; This has been noted to happen when not having an item on the B button while wearing a mask with a B button
+; effect (Blast, Bremen, Kamaro), and calling z64_UseItem.
+;
+; To fix this, we return non-zero if z64_UseItem was called, and if so return from the caller function
+; immediately.
+dpad_handle_caller_return:
+    ; Will be returning from caller function, so restore S0
+    addiu   sp, sp, 0x10
+    lw      s0, 0x0030 (sp)
+
+    ; Restore RA from caller's caller function
+    lw      ra, 0x0034 (sp)
+
+    ; Fix stack for caller and return
+    jr ra
+    addiu   sp, sp, 0x50
