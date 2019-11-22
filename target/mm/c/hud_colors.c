92762e5f507159b3f47603db73267dbde04ec2a5
saneki <s@neki.me>
2019-11-21 18:52:00 -0600

    Custom colors for map

--- target/mm/c/hud_colors.c
+++ target/mm/c/hud_colors.c
@@ -7,14 +7,34 @@ typedef struct {
     uint8_t b;
 } z64_color_rgb8_t;
 
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} z64_color_rgba8_t;
+
 // Infinite magic meter color (blue).
 z64_color_rgb8_t MAGIC_INF_COLOR_CONFIG = { 0x00, 0x00, 0xC8 };
 
 // Magic meter color (green).
 z64_color_rgb8_t MAGIC_COLOR_CONFIG = { 0x00, 0xC8, 0x00 };
 
+// Map color.
+z64_color_rgba8_t MAP_COLOR_CONFIG = { 0x00, 0xFF, 0xFF, 0xA0 };
+
+// Map player cursor color.
+z64_color_rgb8_t MAP_PLAYER_CURSOR_COLOR_CONFIG = { 0xC8, 0xFF, 0x00 };
+
+// Map entrance cursor color.
+z64_color_rgb8_t MAP_ENTRANCE_CURSOR_COLOR_CONFIG = { 0xC8, 0x00, 0x00 };
+
 static uint32_t color_rgb8_to_int(z64_color_rgb8_t color, uint8_t alpha) {
     return (color.r << 24) | (color.g << 16) | (color.b << 8) | alpha;
+}
+
+static uint32_t color_rgba8_to_int(z64_color_rgba8_t color) {
+    return (color.r << 24) | (color.g << 16) | (color.b << 8) | color.a;
 }
 
 uint32_t get_magic_meter_color(bool inf) {
@@ -24,4 +44,18 @@ uint32_t get_magic_meter_color(bool inf) {
     } else {
         return color_rgb8_to_int(MAGIC_COLOR_CONFIG, alpha);
     }
+}
+
+uint32_t get_map_color() {
+    return color_rgba8_to_int(MAP_COLOR_CONFIG);
+}
+
+uint32_t get_map_player_cursor_color() {
+    uint8_t alpha = z64_game.sub_169E8.minimap_alpha & 0xFF;
+    return color_rgb8_to_int(MAP_PLAYER_CURSOR_COLOR_CONFIG, alpha);
+}
+
+uint32_t get_map_entrance_cursor_color() {
+    uint8_t alpha = z64_game.sub_169E8.minimap_alpha & 0xFF;
+    return color_rgb8_to_int(MAP_ENTRANCE_CURSOR_COLOR_CONFIG, alpha);
 }
--- target/mm/src/colors.asm
+++ target/mm/src/colors.asm
@@ -30,3 +30,64 @@ get_magic_meter_color_hook:
 
     jr      ra
     nop
+
+get_map_color_hook:
+    addiu   sp, sp, -0x10
+    sw      ra, 0x0000 (sp)
+    sw      a1, 0x0004 (sp)
+    sw      v1, 0x0008 (sp)
+
+    jal     get_map_color
+    nop
+
+    ; Put return value in T5
+    or      t5, v0, r0
+
+    lw      ra, 0x0000 (sp)
+    lw      a1, 0x0004 (sp)
+    lw      v1, 0x0008 (sp)
+
+    jr      ra
+    addiu   sp, sp, 0x10
+
+get_map_player_cursor_color_hook:
+    addiu   sp, sp, -0x10
+    sw      ra, 0x0000 (sp)
+    sw      v0, 0x0004 (sp)
+    sw      t0, 0x0008 (sp)
+    sw      t8, 0x000C (sp)
+
+    jal     get_map_player_cursor_color
+    nop
+
+    ; Put return value in T7
+    or      t7, v0, r0
+
+    lw      ra, 0x0000 (sp)
+    lw      v0, 0x0004 (sp)
+    lw      t0, 0x0008 (sp)
+    lw      t8, 0x000C (sp)
+
+    jr      ra
+    addiu   sp, sp, 0x10
+
+get_map_entrance_cursor_color_hook:
+    addiu   sp, sp, -0x10
+    sw      ra, 0x0000 (sp)
+    sw      v0, 0x0004 (sp)
+    sw      t0, 0x0008 (sp)
+    sw      t8, 0x000C (sp)
+
+    jal     get_map_entrance_cursor_color
+    nop
+
+    ; Put return value in T5
+    or      t5, v0, r0
+
+    lw      ra, 0x0000 (sp)
+    lw      v0, 0x0004 (sp)
+    lw      t0, 0x0008 (sp)
+    lw      t8, 0x000C (sp)
+
+    jr      ra
+    addiu   sp, sp, 0x10
