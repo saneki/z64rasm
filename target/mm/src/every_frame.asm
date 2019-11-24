9d98941aa0ab0701196ef416b2dc9a347d044ed0
saneki <s@neki.me>
2019-11-24 07:04:21 -0600

    Move damage asm to its own file

--- target/mm/src/every_frame.asm
+++ target/mm/src/every_frame.asm
@@ -37,31 +37,3 @@ before_non_menu_update_hook:
     lw      ra, 0 (sp)
     jr      ra
     addiu   sp, sp, 0x10
-
-before_damage_process_hook:
-    addiu   sp, sp, -0x10
-    sw      ra, 0 (sp)
-    sw      a0, 4 (sp)
-
-    jal     before_damage_process
-    nop
-
-    bnez    v0, @@caller_return
-    nop
-
-    lw      s0, 4 (sp)
-    lw      ra, 0 (sp)
-    jr      ra
-    addiu   sp, sp, 0x10
-
-@@caller_return:
-    ; Will be returning from caller function, so restore S0
-    addiu   sp, sp, 0x10
-    lw      s0, 0x0028 (sp)
-
-    ; Restore RA from caller's caller function
-    lw      ra, 0x002C (sp)
-
-    ; Fix stack for caller and return
-    jr ra
-    addiu   sp, sp, 0x78
