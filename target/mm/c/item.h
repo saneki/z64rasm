3d898297b04cce52a88260068daebb290030e727
saneki <s@neki.me>
2019-11-25 08:27:03 -0600

    Wrap header files in #ifndef

--- target/mm/c/item.h
+++ target/mm/c/item.h
@@ -1,5 +1,10 @@
+#ifndef ITEM_H
+#define ITEM_H
+
 #include <stdbool.h>
 #include <stdint.h>
 
 bool check_c_item_usable(uint8_t c);
 void check_item_usability(bool *dest, uint8_t b, uint8_t c1, uint8_t c2, uint8_t c3);
+
+#endif // ITEM_H
