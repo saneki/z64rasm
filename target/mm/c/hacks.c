6b492bdc4c3a4868872a2a150e99e9ac39f09bb6
saneki <s@neki.me>
2019-12-04 22:44:00 -0600

    Use argument instead of global pointer in previous fix

--- target/mm/c/hacks.c
+++ target/mm/c/hacks.c
@@ -3,7 +3,7 @@
 
 void draw_b_button_icon_color_fix(z64_game_t *game) {
     // Clear the Env color before drawing amounts/text
-    z64_disp_buf_t *db = &(z64_ctxt.gfx->overlay);
+    z64_disp_buf_t *db = &(game->common.gfx->overlay);
     gDPSetEnvColor(db->p++, 0x00, 0x00, 0x00, 0xFF);
 
     z64_DrawBButtonIcon(game);
@@ -11,7 +11,7 @@ void draw_b_button_icon_color_fix(z64_game_t *game) {
 
 void draw_c_button_icons_color_fix(z64_game_t *game) {
     // Clear the Env color before drawing amounts/text
-    z64_disp_buf_t *db = &(z64_ctxt.gfx->overlay);
+    z64_disp_buf_t *db = &(game->common.gfx->overlay);
     gDPSetEnvColor(db->p++, 0x00, 0x00, 0x00, 0xFF);
 
     z64_DrawCButtonIcons(game);
