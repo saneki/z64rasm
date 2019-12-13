b37ba5d2f51b47a17c81baf9c1b3e3d5234700fa
saneki <s@neki.me>
2019-12-13 01:22:08 -0600

    Increase payload size to help future-proof

--- target/mm/src/constants.asm
+++ target/mm/src/constants.asm
@@ -1,6 +1,6 @@
 ; Payload configuration constants
 .definelabel G_PAYLOAD_VROM,  0x03800000
-.definelabel G_PAYLOAD_SIZE,  0x10000
+.definelabel G_PAYLOAD_SIZE,  0x30000
 .definelabel G_PAYLOAD_END,   0x80780000
 .definelabel G_PAYLOAD_ADDR,  (G_PAYLOAD_END - G_PAYLOAD_SIZE)
 
