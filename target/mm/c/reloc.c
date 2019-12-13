ddc0fb6e9e1ba65979d1f4ad5186a73c0be64e64
saneki <s@neki.me>
2019-12-13 01:08:44 -0600

    Reference relocatable functions by VRAM address instead of VROM file offset

--- target/mm/c/reloc.c
+++ target/mm/c/reloc.c
@@ -1,7 +1,8 @@
 #include "z64.h"
 
-void * get_ram_from_player_ovl(z64_player_ovl_table_t *ovl, uint32_t offset) {
-    if (ovl->ram) {
+void * get_ram_from_player_ovl_vram(z64_player_ovl_table_t *ovl, uint32_t vram) {
+    if (ovl->ram && ovl->vram_start <= vram && vram < ovl->vram_end) {
+        uint32_t offset = vram - ovl->vram_start;
         return (void *)((char *)ovl->ram + offset);
     } else {
         return NULL;
--- target/mm/c/reloc.h
+++ target/mm/c/reloc.h
@@ -3,9 +3,15 @@
 
 #include "z64.h"
 
-#define GET_RELOC_FUNC(NAME, OVL) NAME##_proc NAME = get_ram_from_player_ovl(&OVL, NAME##_offset)
+#define GET_RELOC_FUNC(NAME, OVL) ((NAME##_proc)get_ram_from_player_ovl_vram(&OVL, NAME##_vram))
+#define GET_RELOC_PAUSE_FUNC(NAME)  GET_RELOC_FUNC(NAME, z64_0x801D0B70.pause_ovl)
 #define GET_RELOC_PLAYER_FUNC(NAME) GET_RELOC_FUNC(NAME, z64_0x801D0B70.player_ovl)
 
-void * get_ram_from_player_ovl(z64_player_ovl_table_t *ovl, uint32_t offset);
+/* Relocatable player functions */
+#define z64_LinkDamage        GET_RELOC_PLAYER_FUNC(z64_LinkDamage)
+#define z64_LinkInvincibility GET_RELOC_PLAYER_FUNC(z64_LinkInvincibility)
+#define z64_UseItem           GET_RELOC_PLAYER_FUNC(z64_UseItem)
+
+void * get_ram_from_player_ovl_vram(z64_player_ovl_table_t *ovl, uint32_t vram);
 
 #endif // RELOC_H
