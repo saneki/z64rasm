143dbe221802640d069dfe865be045b3f00ef03b
saneki <s@neki.me>
2019-12-14 01:43:09 -0600

    Use versioned structure for D-Pad configuration
    In addition, adds display option for: hide, left side, right side. Also adds const qualifier to certain global variables.

--- assembly/target/mm/c/dpad.h
+++ assembly/target/mm/c/dpad.h
@@ -2,6 +2,7 @@
 #define DPAD_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 // Most of these states get checked by the "UseItem" function itself.
 // We do need to handle some of them though.
@@ -14,7 +15,39 @@ typedef enum {
     DPAD_STATE_TYPE_DISABLED, // 0
     DPAD_STATE_TYPE_ENABLED,  // 1, enabled with DPAD_CONFIG values
     DPAD_STATE_TYPE_DEFAULTS, // 2, enabled with DPAD_DEFAULT values
-} DPAD_STATE_TYPE;
+} dpad_state_type_t;
+
+typedef enum {
+    DPAD_DISPLAY_NONE,
+    DPAD_DISPLAY_LEFT,
+    DPAD_DISPLAY_RIGHT,
+} dpad_display_t;
+
+struct dpad_items {
+    union {
+        uint8_t values[4];              /* 0x0000 */
+        struct {
+            uint8_t du;                 /* 0x0000, up    */
+            uint8_t dr;                 /* 0x0001, right */
+            uint8_t dd;                 /* 0x0002, down  */
+            uint8_t dl;                 /* 0x0003, left  */
+        };
+    };
+};
+
+struct dpad_config {
+    uint32_t version;                   /* 0x0000 */
+    union {
+        struct dpad_items items[4];     /* 0x0004 */
+        struct {
+            struct dpad_items primary;  /* 0x0004 */
+            struct dpad_items alts[3];  /* 0x0008 */
+        };
+    };
+    uint8_t state;                      /* 0x0014 */
+    uint8_t display;                    /* 0x0015 */
+    uint8_t reserved[2];                /* 0x0016 */
+};                                      /* 0x0018 */
 
 void do_dpad_per_game_frame();
 void dpad_init();
