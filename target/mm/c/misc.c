14e44b438c9328bfe74ec010a67dc90a61fbe4b6
saneki <s@neki.me>
2019-12-25 05:09:52 -0600

    Add code and config option for pushblock speed

--- target/mm/c/misc.c
+++ target/mm/c/misc.c
@@ -1,11 +1,26 @@
 #include "misc.h"
+#include "z2.h"
 
 struct misc_config MISC_CONFIG = {
     .magic = MISC_CONFIG_MAGIC,
     .version = 0,
     .ocarina_underwater = 0,
+    .pushblock_mode = PUSHBLOCK_MODE_FAST,
+    .pushblock_speed = 2.0,
 };
 
 bool misc_can_use_ocarina_underwater() {
     return MISC_CONFIG.ocarina_underwater != 0;
+}
+
+f32 misc_get_push_block_speed(z2_actor_t *actor, z2_game_t *game) {
+    switch (MISC_CONFIG.pushblock_mode) {
+        case PUSHBLOCK_MODE_DEFAULT:
+            return 2.0;
+        case PUSHBLOCK_MODE_FAST:
+            return 6.0;
+        case PUSHBLOCK_MODE_CUSTOM:
+        default:
+            return MISC_CONFIG.pushblock_speed;
+    }
 }
--- target/mm/c/misc.h
+++ target/mm/c/misc.h
@@ -2,7 +2,13 @@
 #define MISC_H
 
 #include <stdbool.h>
-#include "types.h"
+#include "z2.h"
+
+enum pushblock_mode {
+    PUSHBLOCK_MODE_DEFAULT,
+    PUSHBLOCK_MODE_FAST,
+    PUSHBLOCK_MODE_CUSTOM,
+};
 
 // Magic number for misc_config: "MISC"
 #define MISC_CONFIG_MAGIC 0x4D495343
@@ -11,9 +17,12 @@ struct misc_config {
     u32 magic;              /* 0x0000 */
     u32 version;            /* 0x0004 */
     u8 ocarina_underwater;  /* 0x0008 */
-    u8 reserved[3];         /* 0x0009 */
-};                          /* 0x000C */
+    u8 pushblock_mode;      /* 0x0009 */
+    u8 reserved[2];         /* 0x000A */
+    f32 pushblock_speed;    /* 0x000C */
+};                          /* 0x0010 */
 
 bool misc_can_use_ocarina_underwater();
+f32 misc_get_push_block_speed(z2_actor_t *actor, z2_game_t *game);
 
 #endif // MISC_H
