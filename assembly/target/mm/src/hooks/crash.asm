7aebb69e1d009e17978b9fe28e43af3a0d0ddc8b
saneki <s@neki.me>
2020-01-01 20:20:13 -0600

    Use VRAM address for crash.asm hook as well

--- assembly/target/mm/src/hooks/crash.asm
+++ assembly/target/mm/src/hooks/crash.asm
@@ -1,10 +1,12 @@
 ;==================================================================================================
 ; Induce a Crash on Pause Menu
 ;==================================================================================================
-;
+
+.headersize(G_KALEIDO_VRAM - G_KALEIDO_FILE)
+
 ; This is used to induce a crash when using the pause menu. It is primarily intended for debugging
 ; on console to get a (working) crash debugger.
 ; Replaces:
 ;   lw      s1, 0x0018 (sp)
-.orga G_KALEIDO_FILE + 0x1262C ; In memory: 0x8075D54C
+.org 0x808286CC ; In RDRAM: 0x8075D54C
     sw      s1, 0x0000 (r0)
