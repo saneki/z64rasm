b3358883fcd50d19053671108298e1e49e4f5d14
saneki <s@neki.me>
2020-01-10 08:57:14 -0600

    Assembly stub optimizations
    Makes better use of delay slots, doesn't back up Saved Registers as compiled
    functions should do this automatically.

--- assembly/target/mm/src/every_frame.asm
+++ assembly/target/mm/src/every_frame.asm
@@ -16,19 +16,11 @@
 ;    nop
 
 before_non_menu_update_hook:
-    addiu   sp, sp, -0x20
+    addiu   sp, sp, -0x18
     sw      ra, 0x0010 (sp)
-
-    ; Store modified regs prior to hook
-    sw      s0, 0x0014 (sp)
-    sw      s1, 0x0018 (sp)
 
     jal before_non_menu_update
     nop
-
-    ; Restore modified regs
-    lw      s0, 0x0014 (sp)
-    lw      s1, 0x0018 (sp)
 
     ; Displaced code
     lw      t6, 0x0A74 (s0)
@@ -37,4 +29,4 @@ before_non_menu_update_hook:
     lw      ra, 0x0010 (sp)
 
     jr      ra
-    addiu   sp, sp, 0x20
+    addiu   sp, sp, 0x18
