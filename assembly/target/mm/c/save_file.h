7f226afb1a71c9240e0e2daa957cbefe41bb0de8
saneki <s@neki.me>
2020-01-12 09:13:20 -0600

    Update save config location and is now written to flash

--- assembly/target/mm/c/save_file.h
+++ assembly/target/mm/c/save_file.h
@@ -2,6 +2,7 @@
 #define SAVE_FILE_H
 
 #include "quest_item_storage.h"
+#include "z2.h"
 
 #define SAVE_FILE_CONFIG_MAGIC 0x53415645
 
@@ -11,7 +12,11 @@ struct save_file_config {
     struct quest_item_storage quest_storage;         /* 0x0008 */
 };                                                   /* 0x001A */
 
+#define SAVE_FILE_RAM    (z2_file_addr + 0x1E00)
+#define SAVE_FILE_CONFIG (*(struct save_file_config*) SAVE_FILE_RAM)
+
 struct save_file_config* save_file_get_config(void);
 struct quest_item_storage* save_file_get_quest_item_storage(void);
+void save_file_init(void);
 
 #endif // SAVE_FILE_H
