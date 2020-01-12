0f61bf7d6bbb0aec4ae30cc4c989c5e24645d803
saneki <s@neki.me>
2020-01-12 11:17:45 -0600

    Fix buggy behaviour for quest items on the wrong slot
    No longer draws next quest item icon if quest item is on the wrong slot. Also
    does not try to cycle to the next item.

--- target/mm/src/pause.asm
+++ target/mm/src/pause.asm
@@ -1,19 +1,22 @@
 pause_menu_select_item_draw_icon_hook:
     addiu   sp, sp, -0x20
-    sw      ra, 0x0018 (sp)
+    sw      ra, 0x001C (sp)
 
-    ; Move parameter 5 (vertices index for Quadrangle)
+    ; Parameter 5 (inventory slot index)
+    sw      s1, 0x0010 (sp)
+
+    ; Parameter 6 (vertices index for Quadrangle)
     lw      at, 0x0030 (sp)
-    sw      at, 0x0010 (sp)
+    sw      at, 0x0014 (sp)
 
-    ; Parameter 6 (Vertex buffer index)
-    sw      s2, 0x0014 (sp)
+    ; Parameter 7 (Vertex buffer index)
+    sw      s2, 0x0018 (sp)
 
     jal     pause_menu_select_item_draw_icon
     ; Overwrite A1 with item Id in V0 (we can calculate A1 with it later)
     or      a1, v0, r0
 
-    lw      ra, 0x0018 (sp)
+    lw      ra, 0x001C (sp)
 
     jr      ra
     addiu   sp, sp, 0x20
