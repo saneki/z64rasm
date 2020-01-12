fba81e56a54b8234338d4b07c8dea8952e635e49
saneki <s@neki.me>
2020-01-12 07:19:21 -0600

    Add actor types, rename actor function & types

--- target/mm/c/actor.c
+++ target/mm/c/actor.c
@@ -1,5 +1,5 @@
 #include "z2.h"
 
-z2_actor_t* spawn_actor(z2_game_t *game, u8 id, z2_xyzf_t pos, z2_rot_t rot, u16 var) {
+z2_actor_t* actor_spawn(z2_game_t *game, u8 id, z2_xyzf_t pos, z2_rot_t rot, u16 var) {
     return z2_SpawnActor(&(game->actor_ctxt), game, id, pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, var);
 }
--- target/mm/c/actor.h
+++ target/mm/c/actor.h
@@ -3,6 +3,6 @@
 
 #include "z2.h"
 
-z2_actor_t* spawn_actor(z2_game_t *game, u8 id, z2_xyzf_t pos, z2_rot_t rot, u16 var);
+z2_actor_t* actor_spawn(z2_game_t *game, u8 id, z2_xyzf_t pos, z2_rot_t rot, u16 var);
 
 #endif // ACTOR_H
