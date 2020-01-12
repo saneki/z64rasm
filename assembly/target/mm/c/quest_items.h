bbfa748e2277afdcbe414214b41c5829f61e458f
saneki <s@neki.me>
2020-01-12 10:46:03 -0600

    Move and rename function for getting expected slot of quest item

--- assembly/target/mm/c/quest_items.h
+++ assembly/target/mm/c/quest_items.h
@@ -1,6 +1,7 @@
 #ifndef QUEST_ITEMS_H
 #define QUEST_ITEMS_H
 
+#include <stdbool.h>
 #include "z2.h"
 
 // Check if an inventory slot is a quest items slot.
@@ -9,5 +10,6 @@
 void quest_items_after_receive(u8 item);
 void quest_items_after_removal(u8 item, u8 slot);
 void quest_items_after_song_of_time_clear(void);
+bool quest_items_get_slot(int *slot, u8 item);
 
 #endif // QUEST_ITEMS_H
