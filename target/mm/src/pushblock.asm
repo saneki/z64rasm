257aa4fd6c10103080fa4be58f837fbc27880532
saneki <s@neki.me>
2020-01-12 06:58:15 -0600

    Update iceblock push speed to properly use clamp value

--- target/mm/src/pushblock.asm
+++ target/mm/src/pushblock.asm
@@ -24,22 +24,23 @@ misc_get_push_block_speed_hook:
     addiu   sp, sp, 0x20
 
 misc_get_iceblock_push_speed_hook:
-    addiu   sp, sp, -0x20
+    addiu   sp, sp, -0x28
     sw      ra, 0x0010 (sp)
     swc1    f0, 0x0014 (sp)
 
     jal     misc_get_iceblock_push_speed
     addiu   a2, sp, 0x18
 
-    ; Move return values to F6 and F18 from stack
+    ; Move return values to F6, F18 and F12 from stack
     lwc1    f6, 0x0018 (sp)
     lwc1    f18, 0x001C (sp)
+    lwc1    f12, 0x0020 (sp)
 
     lw      ra, 0x0010 (sp)
     lwc1    f0, 0x0014 (sp)
 
     jr      ra
-    addiu   sp, sp, 0x20
+    addiu   sp, sp, 0x28
 
 misc_get_great_bay_temple_faucet_speed_hook:
     addiu   sp, sp, -0x20
