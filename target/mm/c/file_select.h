217b51dd47ef52f66aaf93e5e26b9b16aa522554
saneki <s@neki.me>
2020-02-18 13:02:49 -0600

    Wrap hash symbol table in struct, no longer static
    Structure includes symbol count for verification.

---
+++ target/mm/c/file_select.h
@@ -0,0 +1,13 @@
+#ifndef FILE_SELECT_H
+#define FILE_SELECT_H
+
+#include "types.h"
+
+#define HASH_SYMBOL_COUNT 0x40
+
+struct hash_icons {
+    u16 count;
+    u8 symbols[HASH_SYMBOL_COUNT];
+};
+
+#endif // FILE_SELECT_H
