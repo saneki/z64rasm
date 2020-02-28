c9b53af53985c1d9e8d69031d5fc1c3bb84fa4a6
saneki <s@neki.me>
2020-02-28 03:18:58 -0600

    Move and rename Song of Time hook

--- target/mm/src/hooks/savedata.asm
+++ target/mm/src/hooks/savedata.asm
@@ -9,3 +9,13 @@
 ;   jr      ra
 .org 0x80145464
     j       savedata_after_load_hook
+
+; Hook after preparing savedata.
+; Replaces:
+;   lw      s5, 0x0028 (sp)
+;   jr      ra
+;   addiu   sp, sp, 0x30
+.org 0x80144568
+    or      a0, s5, r0
+    j       savedata_after_prepare_hook
+    lw      s5, 0x0028 (sp)
--- target/mm/src/savedata.asm
+++ target/mm/src/savedata.asm
@@ -2,3 +2,8 @@ savedata_after_load_hook:
     lw      a0, 0x0000 (sp)
     j       savedata_after_load
     lw      a1, 0x0004 (sp)
+
+savedata_after_prepare_hook:
+    j       savedata_after_prepare
+    ; Displaced code (stack adjustment)
+    addiu   sp, sp, 0x30
