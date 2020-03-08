4649bfdcff00d72e85b2953810374dba69f9c621
saneki <s@neki.me>
2020-03-07 20:47:14 -0600

    Comment out older hacks for normal masks on D-Pad
    These hacks are likely preventing mask storage from working.

--- assembly/target/mm/src/hacks.asm
+++ assembly/target/mm/src/hacks.asm
@@ -7,21 +7,21 @@
 ; Remove C button check for equipped mask, fixes using B button item (usually sword).
 ; Replaces:
 ;   jal     0x8074D19C
-.org 0x8082FE84 ; In RDRAM: 0x8074D314
-    addiu   v0, r0, 1
+;.org 0x8082FE84 ; In RDRAM: 0x8074D314
+;    addiu   v0, r0, 1
 
 ; For some items, if UseItem is called without said item being on the C-buttons,
 ; it will be automatically called again to unequip. Prevents this.
 ; Replaces:
 ;   jal     0x8074EE20
-.org 0x8082FF1C ; In RDRAM: 0x8074D3AC
-    nop
+;.org 0x8082FF1C ; In RDRAM: 0x8074D3AC
+;    nop
 
 ; Remove relocations.
-.org 0x8085E908
-    .dw 0x00000000 ; Replaces: 0x440023F4
-.org 0x8085E910
-    .dw 0x00000000 ; Replaces: 0x4400248C
+;.org 0x8085E908
+;    .dw 0x00000000 ; Replaces: 0x440023F4
+;.org 0x8085E910
+;    .dw 0x00000000 ; Replaces: 0x4400248C
 
 ;==================================================================================================
 ; Draw C Button Amounts Fix
